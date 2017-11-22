4bfecbb3eaf0a778469843dc90d3605551747405
kounelios13 <pkounelios@gmail.com>
2017-11-22 11:11:13 +0200

    Removed lockr dependency from index renderer

--- src/public/scripts/renderers/index.js
+++ src/public/scripts/renderers/index.js
@@ -1,7 +1,4 @@
 const {ipcRenderer} = require('electron');
-const lockr = require('lockr');
-let appointments = lockr.get('appointments')||[];
-let createAppointment,viewAppointments;
 window.addEventListener('DOMContentLoaded',function(){
     createAppointment = document.getElementById('create-appointment');
     viewAppointments = document.getElementById('view-appointments');
